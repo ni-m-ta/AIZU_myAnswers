e774b8cd1dbf96888d7a2c83621c248d1294b917
nita <tatsuyanisato@gmail.com>
2022-06-23 00:07:41 +0900

    transformation

--- ITP1_9_D_Transformation.py
+++ ITP1_9_D_Transformation.py
@@ -1,11 +1,22 @@
 # Write a program which performs a sequence of commands to a given string str. The command is one of
 
-commands_list=[]
+def rep(w,n1,n2,c):
+    w=w[:n1]+c+w[n2+1:]
+    return w
 
+def rev(w,n1,n2,n3):
+    w=w[:n1]+w[n2-n3:n1-n3-1:-1]+w[n2+1:]
+    return w
+
+def pri(w,n1,n2):
+    w=w[n1:n2+1]
+    return w
+
+commands_list=[]
 word=input()
 times=int(input())
 print_count=0
-prited_words=[]
+printed_words=[]
 
 for i in range(times):
     command=list(map(str,input().split()))
@@ -15,7 +26,7 @@ for i in range(times):
     if commands_list[i][0]=='replace':
         word=rep(word,int(commands_list[i][1]),int(commands_list[i][2]),commands_list[i][3])
     elif commands_list[i][0]=='reverse':
-        word=rev(word,int(commands_list[i][1]),int(commands_list[i][2]))
+        word=rev(word,int(commands_list[i][1]),int(commands_list[i][2]),len(word))
     else:
         printed_words.append(pri(word,int(commands_list[i][1]),int(commands_list[i][2])))
         print_count+=1
