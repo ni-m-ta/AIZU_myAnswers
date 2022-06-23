99f47d72fed436bc351fdfc2f2daf9cae049fd52
apple <user@NiTanoMacBook-Pro.local>
2022-06-23 14:47:29 +0900

    con

--- ITP1_10_B_Triangle.py
+++ ITP1_10_B_Triangle.py
@@ -1,11 +1,11 @@
 #For given two sides of a triangle a and b and the angle C between them, calculate the following properties:
 
 import math
-import decimal
 
 a,b,c=map(float,input().split())
 
-s=1/2*a*b*math.sin(math.radians(c))
+
+s=1/2*a*b*(math.sin(math.radians(c)))
 
 l=a+b+(math.sin(math.radians(c))*a)
 
